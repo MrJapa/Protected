352bf870ea7be3d14436fd9a0e23b315d16cf36a
J. Japa <jjv@live.dk>
2022-02-10 09:25:56 +0100

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Klasse1
+{
+    protected int A;
+    protected int B;
+}
+class Klasse2 : Klasse1
+{
+    static void Main()
+    {
+        //Her kan klasse 2 godt tilgå de protectede ints fra Klasse 1, da klasse 2 er derived fra direkte fra klasse 1.
+        var c = new Klasse2();
+        c.A = 1;
+        c.B = 2;
+        Console.WriteLine($"Int A: {c.A} Int B: {c.B}");
+    }
+}
+
+/* Her kan klasse 3 ikke tilgå de protectede ints fra Klasse 1, da den ikke er derived.
+class Klasse3
+{
+    static void Test()
+    {
+        var d = new Klasse3();
+        d.A = 1;
+        d.B = 2;
+        Console.WriteLine($"Int A: {d.A} Int B: {d.B}");
+    }
+}
+*/
